18f239c3d0f8c72f6b44b59c402bc70e7c66c166
Oliver <1155175373@link.cuhk.edu.hk>
2022-07-08 20:28:02 +0800

    v1 build

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("frontend/build"));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`app running on ${PORT}`));
