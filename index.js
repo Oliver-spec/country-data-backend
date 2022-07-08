c8fca1ed80ba7c5880d8f12743704926e92ef6cf
Oliver <1155175373@link.cuhk.edu.hk>
2022-07-08 21:05:19 +0800

    remove build from gitignore

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const app = express();
 
 app.use(express.static("frontend/build"));
 
-app.get("/", (req, res) => res.send("hello"));
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`app running on ${PORT}`));
