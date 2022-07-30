352ebd431924a151f8ad62e5186e8eb5d623f185
Oliver <1155175373@link.cuhk.edu.hk>
2022-07-31 00:23:43 +0800

    call api from backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,11 @@
 import axios from "axios";
 
-const api_proxy = "https://express-api-proxy-01010.herokuapp.com";
-
 function getAll() {
-  return axios
-    .get("https://restcountries.com/v3.1/all")
-    .then((response) => response.data);
+  return axios.get("/allCountries").then((response) => response.data);
 }
 
 function getWeather(capital) {
-  return axios
-    .post(`${api_proxy}/weather`, {
-      URL: `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&APPID=api_key`,
-    })
-    .then((response) => response.data);
+  return axios.get(`/weather/${capital}`).then((response) => response.data);
 }
 
 function getDialCodes() {
--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+require("dotenv").config();
+// console.log(process.env);
 const express = require("express");
 const fs = require("fs-extra");
 const cors = require("cors");
+const axios = require("axios").default;
 
 const app = express();
 
 app.use(express.static("frontend/build"));
 app.use(cors());
 
+// get dialcodes
 app.get("/data", (req, res) => {
   const dialCodes = fs.readJSONSync("./database/CountryCodes.json");
   // console.log(dialCodes);
-  res.send(dialCodes);
+  res.json(dialCodes);
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`app running on ${PORT}`));
+// 3rd party api call to get country data
+app.get("/allCountries", (req, res) => {
+  axios
+    .get("https://restcountries.com/v3.1/all")
+    .then((countries) => res.json(countries.data));
+});
+
+// 3rd party weather api
+app.get("/weather/:capital", (req, res) => {
+  const capital = req.params.capital;
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&APPID=${process.env.WEATHER_API_KEY}`
+    )
+    .then((weather) => res.json(weather.data));
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`app running on ${process.env.PORT}`)
+);
